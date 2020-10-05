61bfc71853cc2b2e781503a6dee8443c5249181b
Palina Cetin <fpe@tut.by>
2020-10-05 19:29:26 +0300

    feat: create solution two tasks more

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (Array.isArray(arr)) {
+      if (arr.length === 0) {
+        return 1;  
+      }
+      else {
+        return 1+arr.reduce( (max, curr) => {
+          return Math.max(max, this.calculateDepth(curr));
+        }, 0);
+      }      
+    } else {
+      return 0;
+    }    
   }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  
+  chainArray:[],
+  
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   return (this.chainArray.length); 
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value=' ') {        
+    value = value + '';           
+    this.chainArray.push(`( ${value} )`);    
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if ( position <= 0 || position%1 != 0 || position >(this.chainArray.length) || isNaN(position) ) {
+      this.chainArray = [];
+      throw new Error('Error');
+    }
+    else {
+      this.chainArray.splice(position-1, 1);
+      console.log(this.chainArray);
+    }
+    return this;    
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArray.reverse();
+    return this;    
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let chainString = '';
+    if (this.chainArray.length === 0) {
+      this.chainArray = [];
+      throw new Error('error');
+    } 
+    for (let i=0; i<this.chainArray.length; i++) {
+      let value = this.chainArray[i];
+      if (i === 0) {
+        chainString += value;
+      }
+      else {
+        chainString += `~~${value}`;
+      } 
+    }    
+    this.chainArray = [];
+    return chainString; 
+  }  
 };
 
 module.exports = chainMaker;
