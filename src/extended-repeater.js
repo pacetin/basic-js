a70c06148eb13c0b0f823c085032c55a4e048bb5
Palina Cetin <fpe@tut.by>
2020-10-07 15:59:57 +0300

    feat: three more tasks solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, {repeatTimes=0, separator='+', addition='', additionRepeatTimes=0, additionSeparator='|'}) {
+  str = str + '';
+  separator = separator + '';
+  addition = addition + '';
+  additionSeparator = additionSeparator + '';
+
+  let additionString = '';
+  let mainString = '';
+
+  if (addition !== '') {
+    if (additionRepeatTimes !== 0 ) {
+      additionString += addition;
+      let n=2;
+      while (n<=additionRepeatTimes) {
+        additionString +=additionSeparator + addition;
+        n++;
+      }
+    } else if (additionRepeatTimes === 0 || additionRepeatTimes === 1) {
+      additionString += addition;
+    }
+  }
+  
+
+  if (str !== '') {
+    if (repeatTimes !== 0 ) {
+      mainString += str + additionString;
+      let n=2;
+      while (n<=repeatTimes) {
+        mainString += separator + str + additionString;
+        n++;
+      }
+    } else if (repeatTimes === 0 || repeatTimes ===1) {
+      mainString += str + additionString;
+    }
+  }
+  
+
+  
+  //console.log(additionString);
+  return mainString;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,165 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct=true) {
+    this.direct = direct;
+
+    this.VIGENERE_TABLE = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 8,
+    'j': 9,
+    'k': 10,
+    'l': 11,
+    'm': 12,
+    'n': 13,
+    'o': 14,
+    'p': 15,
+    'q': 16,
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'v': 21,
+    'w': 22,
+    'x': 23,
+    'y': 24,
+    'z': 25    
+    };
+
+    this.VIGENERE_TABLE_REVERT = {
+    0 :'a',
+    1 :'b',
+    2 :'c',
+    3 :'d',
+    4 :'e',
+    5 :'f',
+    6 :'g',
+    7 :'h',
+    8 :'i',
+    9 :'j',
+    10 :'k',
+    11 :'l',
+    12 :'m',
+    13 :'n',
+    14 :'o',
+    15 :'p',
+    16 :'q',
+    17 :'r',
+    18 :'s',
+    19 :'t',
+    20 :'u',
+    21 :'v',
+    22 :'w',
+    23 :'x',
+    24 :'y',
+    25 :'z'   
+    };
+  }  
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('custom error');
+    }
+
+    const messageArray = message.toLowerCase().split('');
+    key = key.toLowerCase();
+    const messageNumberArray = [];
+    const specialSymbols = {}
+    for (let i=0; i<messageArray.length; i++) {
+      let letter = messageArray[i]
+      if (letter in this.VIGENERE_TABLE) {
+        messageNumberArray.push(this.VIGENERE_TABLE[letter]);
+      } else { 
+        specialSymbols[i] = letter;
+      }
+    }
+
+    const keyNumberArray = [];
+    let j=0;
+    let k=0;
+    while (j<messageNumberArray.length) {
+      if (k>=key.length) {
+        k=0;
+      }      
+      keyNumberArray.push(this.VIGENERE_TABLE[key[k]]);
+      k++;
+      j++;           
+    }
+    
+    const cipherArray = [];
+    for (let i=0; i<messageNumberArray.length; i++) {
+      cipherArray.push(this.VIGENERE_TABLE_REVERT[this.getModulSum(messageNumberArray[i], keyNumberArray[i])]);
+    }    
+
+    for (let prop in specialSymbols) {
+      cipherArray.splice(prop, 0, specialSymbols[prop]);
+    }
+
+    const cipherString = this.direct ? cipherArray.join('').toUpperCase() : cipherArray.reverse().join('').toUpperCase();
+    
+    return cipherString;
   }
-}
+
+  getModulSum(value1, value2) {
+    let sum = value1+value2;
+    return sum<26?sum:sum-26;
+  }
+
+  getModulDif(value1, value2) {
+    let dif = value1-value2;
+    return dif>=0?dif:dif+26;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('custom error');
+    }
+
+    key = key.toLowerCase();
+    const messageArray = message.toLowerCase().split('');
+    const messageNumberArray = [];
+    const specialSymbols = {}
+    for (let i=0; i<messageArray.length; i++) {
+      let letter = messageArray[i]
+      if (letter in this.VIGENERE_TABLE) {
+        messageNumberArray.push(this.VIGENERE_TABLE[letter]);
+      } else { 
+        specialSymbols[i] = letter;
+      }
+    }
+
+    const keyNumberArray = [];
+    let j=0;
+    let k=0;
+    while (j<messageNumberArray.length) {
+      if (k>=key.length) {
+        k=0;
+      }      
+      keyNumberArray.push(this.VIGENERE_TABLE[key[k]]);
+      k++;
+      j++;           
+    }
+    
+    const cipherArray = [];
+    for (let i=0; i<messageNumberArray.length; i++) {
+      cipherArray.push(this.VIGENERE_TABLE_REVERT[this.getModulDif(messageNumberArray[i], keyNumberArray[i])]);
+    }    
+
+    for (let prop in specialSymbols) {
+      cipherArray.splice(prop, 0, specialSymbols[prop]);
+    }
+
+    const cipherString = this.direct ? cipherArray.join('').toUpperCase() : cipherArray.reverse().join('').toUpperCase();
+    
+    return cipherString;
+  }
+};
 
 module.exports = VigenereCipheringMachine;
